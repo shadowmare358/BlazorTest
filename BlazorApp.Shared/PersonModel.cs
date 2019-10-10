35753f45dabf412a0296f1d69017e9ff65bcec42
Mike <shadowmare@windowslive.com>
2019-10-10 22:47:53 +0200

    Dodaj pliki projektów.

---
+++ BlazorApp.Shared/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Decimal AccountBalance { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
---
+++ BlazorApp1.Server/Controller/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlazorApp1.Server.Controller
+{
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        [Route("all")]
+        public IEnumerable<PersonModel> GetPeople()
+        {
+            var output = new List<PersonModel>
+            {
+                new PersonModel {FirstName="Tim", LastName="Correy", AccountBalance=19.45M },
+                new PersonModel {FirstName="Mary", LastName="Jones", AccountBalance=105.87M },
+                new PersonModel {FirstName="John", LastName="Smith", AccountBalance=115.32M }
+            };
+            return output;
+        }
+        [HttpPost]
+        public void Post(PersonModel person)
+        {
+
+        }
+
+      
+    }
+}
